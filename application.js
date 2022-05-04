6f06a5b06803ff416f69454f82a909b6e24ffa07
felipe cook <felipepcook@gmail.com>
2022-05-04 13:23:14 -0400

    + operate function

--- application.js
+++ application.js
@@ -16,3 +16,24 @@ function multiply(numberA, numberB) {
 function divide(numberA, numberB) {
   return numberA / numberB;
 }
+
+function operate(operator, numberA, numberB) {
+  switch (operator) {
+    case '+':
+      return add(numberA, numberB);
+  
+    case '-':
+      return subtract(numberA, numberB);
+
+    case '/':
+      return divide(numberA, numberB);
+
+    case '*':
+      return multiply(numberA, numberB);
+
+    default:
+      alert('You entered an operator that is not recognized by this calculator, please use +, -, *, or /')
+      break;
+  }
+  
+}
