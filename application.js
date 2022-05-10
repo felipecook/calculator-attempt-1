6ae94c0c1c61e3cc50c31b409c75069afe663479
felipe cook <felipepcook@gmail.com>
2022-05-09 20:09:36 -0400

    updated equate func with simpler logic, works great

--- application.js
+++ application.js
@@ -56,56 +56,98 @@ function clearCalculator() {
 
 function equate (){
   const equation = display.innerHTML.trim();
-  
-  // console.log(equation);
-  // console.log(equation.length)
 
-  if (equation.length > 5) {
-    let flag = false;
-    for (let index = 0; index < equation.length; index++) {
-      const element = equation[index];
-      if ((element === '+' || element === '-' || element === '/' || element === '*') && flag === true) {
-        let firstNumber = 0;
-        let secondNumber = 0;
-        let operator = 0;
-        
-        for (let jindex = 0; jindex <= index; jindex++) {
-          secondElement = equation[jindex]; 
-          if (secondElement === '+' || secondElement === '-' || secondElement === '/' || secondElement === '*') {
-            firstNumber = equation.slice(0, jindex);
-            operator = equation[jindex];
-            secondNumber = equation.slice(jindex + 1, index);
-            firstNumber = trimExtra(firstNumber);
-            secondNumber = trimExtra(secondNumber);
+  let numOfOperators = 0;
+  let positionOfOperators = {};
+  let counter = 0;
+  //First we find all the indicies of the operators in the string
+  for (let index = 0; index < equation.length; index++) {
+    const element = equation[index];
+    
 
-            console.log("The first number is: " +   parseInt(firstNumber));
-            console.log("The second number is: " + secondNumber);
-            console.log("The operator is: " + operator);
-            console.log("The index is: " + index);
-
-            display.innerHTML = operate(operator, firstNumber, secondNumber) + " " + equation.slice(index);
-
-            break;
-  
-          }
-          
-        }
-
-      }
-      if(element === '+' || element === '-' || element === '/' || element === '*') {
-        flag = true;
-      }
-      
-      
+    if (element === '+' || element === '-' || element === '/' || element === '*'){
+      numOfOperators++;
+      positionOfOperators[counter] = index;
+      counter++;
     }
 
-  } else {
-    const answer = '';
-    console.log(equation[2]);
-    console.log(operate(equation[2], equation[0], equation[4]));
-    // answer = operate(equation[2], equation[0], equation[4]);
-    display.innerHTML = operate(equation[2], equation[0], equation[4]);
+    
   }
+
+  // if we have more than one operator, we need to slice all the way up to that second operator, perform operations and then return an answer with all the rest of the unprocessed string added.
+  if(numOfOperators > 1){
+    let firstNumber = equation.slice(0, positionOfOperators[0]);
+    let secondNumber = equation.slice(positionOfOperators[0] + 1, positionOfOperators[1]);
+    let operator = equation[positionOfOperators[0]];
+
+    firstNumber = trimExtra(firstNumber);
+    secondNumber = trimExtra(secondNumber);
+
+    display.innerHTML = operate(operator, firstNumber, secondNumber) + " " + equation.slice(positionOfOperators[1]);
+
+  } else {
+    let firstNumber = equation.slice(0, positionOfOperators[0]);
+    let secondNumber = equation.slice(positionOfOperators[0] + 1, equation.length);
+    let operator = equation[positionOfOperators[0]];
+
+    firstNumber = trimExtra(firstNumber);
+    secondNumber = trimExtra(secondNumber);
+
+    display.innerHTML = operate(operator, firstNumber, secondNumber);
+
+  }
+
+
+
+
+  
+
+  // if (equation.length > 5) {
+  //   let flag = false;
+  //   for (let index = 0; index < equation.length; index++) {
+  //     const element = equation[index];
+  //     if ((element === '+' || element === '-' || element === '/' || element === '*') && flag === true) {
+  //       let firstNumber = 0;
+  //       let secondNumber = 0;
+  //       let operator = 0;
+        
+  //       for (let jindex = 0; jindex <= index; jindex++) {
+  //         secondElement = equation[jindex]; 
+  //         if (secondElement === '+' || secondElement === '-' || secondElement === '/' || secondElement === '*') {
+  //           firstNumber = equation.slice(0, jindex);
+  //           operator = equation[jindex];
+  //           secondNumber = equation.slice(jindex + 1, index);
+  //           firstNumber = trimExtra(firstNumber);
+  //           secondNumber = trimExtra(secondNumber);
+
+  //           console.log("The first number is: " +   parseInt(firstNumber));
+  //           console.log("The second number is: " + secondNumber);
+  //           console.log("The operator is: " + operator);
+  //           console.log("The index is: " + index);
+
+  //           display.innerHTML = operate(operator, firstNumber, secondNumber) + " " + equation.slice(index);
+
+  //           break;
+  
+  //         }
+          
+  //       }
+
+  //     }
+  //     if(element === '+' || element === '-' || element === '/' || element === '*') {
+  //       flag = true;
+  //     }
+      
+      
+  //   }
+
+  // } else {
+  //   const answer = '';
+  //   console.log(equation[2]);
+  //   console.log(operate(equation[2], equation[0], equation[4]));
+  //   // answer = operate(equation[2], equation[0], equation[4]);
+  //   display.innerHTML = operate(equation[2], equation[0], equation[4]);
+  // }
 
 
 }
